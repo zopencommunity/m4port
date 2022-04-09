1ef43f61102dae83faa3f73ad71c9435cd3804b9
Mike Fulton <mikefultonpersonal@gmail.com>
2022-04-09 17:50:56 -0400

    clean up the code a bit for the whole process to build 'clean'

--- m4build.sh
+++ m4build.sh
@@ -5,88 +5,101 @@
 #  - ensure you have sourced setenv.sh, e.g. . ./setenv.sh
 #  - ensure you have GNU make installed (4.1 or later)
 #  - ensure you have access to c99
+#  - ensure you have an m4 bootstrap program available under M4_BOOTSTRAP_ROOT
 #  - either pre-install the M4 tar ball into M4_ROOT or have curl/gunzip installed for auto-download
 #
+set -x
+
 if [ "${M4_ROOT}" = '' ]; then
 	echo "Need to set M4_ROOT - source setenv.sh" >&2
+	exit 16
+fi
+if [ "${M4_BOOTSTRAP_ROOT}" = '' ]; then
+	echo "Need to set M4_BOOTSTRA_PROOT - source setenv.sh" >&2
 	exit 16
 fi
 if [ "${M4_VRM}" = '' ]; then
 	echo "Need to set M4_VRM - source setenv.sh" >&2
 	exit 16
 fi
+if [ "${M4_BRANCH}" = '' ]; then
+	echo "Need to set M4_BRANCH - source setenv.sh" >&2
+	exit 16
+fi
 
-make --version >/dev/null 2>&1 
-if [ $? -gt 0 ]; then
+if ! make --version >/dev/null 2>&1 ; then
 	echo "You need GNU Make on your PATH in order to build M4" >&2
 	exit 16
 fi
 
-whence c99 >/dev/null
-if [ $? -gt 0 ]; then
+if ! whence c99 >/dev/null ; then
 	echo "c99 required to build M4. " >&2
 	exit 16
 fi
 
-MY_ROOT="${PWD}"
+if ! ${M4_BOOTSTRAP_ROOT}/bin/m4 --version >/dev/null 2>&1; then
+	echo "bootstrap m4 required to build M4. " >&2
+	exit 16
+fi
 
-if [ "${M4_VRM}" != "m4" ]; then
+MY_ROOT=$(cd dirname $0; echo $PWD)
+cd "${M4_ROOT}" || exit 99
+
+if [ "${M4_VRM}" = "m4" ]; then
+	if ! [ -d "${M4_VRM}" ] ; then 
+		if ! git clone "${AUTOTOOLS_MIRROR}/${M4_VRM}.git" ; then
+			exit 4
+		fi
+	fi
+else
 	# Non-dev - get the tar file
-	rm -rf "${M4_ROOT}/${M4_VRM}"
-	mkdir -p "${M4_ROOT}/${M4_VRM}"
-	if [ $? -gt 0 ]; then
+
+	rm -rf "${M4_VRM}"
+	if ! mkdir -p "${M4_VRM}"; then
 		echo "Unable to make root M4 directory: ${M4_ROOT}/${M4_VRM}" >&2
 		exit 16
 	fi
-	cd "${M4_ROOT}"
-	M4_ROOT="${PWD}"
 
 	if ! [ -f "${M4_VRM}.tar" ]; then
 		URL="http://ftp.gnu.org/gnu/m4/"
 		echo "m4 tar file not found. Attempt to download with curl" 
-		whence curl >/dev/null
-		if [ $? -gt 0 ]; then
-			echo "curl not installed. You will need to upload M4, or install curl/gunzip from ${URL}" >&2
+		if ! whence curl >/dev/null ; then
+			echo "curl not installed. You will need to upload M4, or install curl/gunzip from ${M4_URL}" >&2
 			exit 16
 		fi	
-		whence gunzip >/dev/null
-		if [ $? -gt 0 ]; then
-			echo "gunzip required to unzip M4. You will need to upload M4, or install curl/gunzip from ${URL}" >&2
+		if ! whence gunzip >/dev/null ; then
+			echo "gunzip required to unzip M4. You will need to upload M4, or install curl/gunzip from ${M4_URL}" >&2
 			exit 16
 		fi	
-		(rm -f ${M4_VRM}.tar.gz; curl -s --output ${M4_VRM}.tar.gz http://ftp.gnu.org/gnu/m4/${M4_VRM}.tar.gz)
-		rc=$?
-		if [ $rc -gt 0 ]; then
+		if ! (rm -f ${M4_VRM}.tar.gz; curl -s --output ${M4_VRM}.tar.gz http://ftp.gnu.org/gnu/m4/${M4_VRM}.tar.gz) ; then
 			echo "curl failed with rc $rc when trying to download ${M4_VRM}.tar.gz" >&2
 			exit 16
 		fi	
 		chtag -b ${M4_VRM}.tar.gz
-		gunzip ${M4_VRM}.tar.gz
-		rc=$?
-		if [ $rc -gt 0 ]; then
+		if ! gunzip ${M4_VRM}.tar.gz ; then 
 			echo "gunzip failed with rc $rc when trying to unzip ${M4_VRM}.tar.gz" >&2
 			exit 16
 		fi	
 	fi
 
-	tar -xf "${M4_VRM}.tar"
-	if [ $? -gt 0 ]; then
-		echo "Unable to make untar M4 drop: ${M4_VRM}" >&2
+	tar -xf "${M4_VRM}.tar" 2>/dev/null
+#
+# TBD: figure out how to untar the tar file without errors about setting uid/gid
+#
+	if [ $? -gt 1 ]; then
+		echo "Unable to untar M4 drop: ${M4_VRM}" >&2
 		exit 16
 	fi
-else
-	cd "${M4_ROOT}"
-	M4_ROOT="${PWD}"
 fi
-chtag -R -h -t -cISO8859-1 "${M4_VRM}"
-if [ $? -gt 0 ]; then
+
+if ! chtag -R -h -t -cISO8859-1 "${M4_VRM}"; then
 	echo "Unable to tag M4 directory tree as ASCII" >&2
 	exit 16
 fi
 
 DELTA_ROOT="${PWD}"
 
-cd "${M4_ROOT}/${M4_VRM}"
+cd "${M4_ROOT}/${M4_VRM}" || exit 99
 
 #
 # Apply patches
@@ -104,6 +117,8 @@ if [ "${M4_VRM}" = "m4-1.4.19" ]; then
 	fi
 fi
 
+export PATH="${M4_BOOTSTRAP_ROOT}/bin:${PATH}"
+
 if [ "${M4_VRM}" = "m4" ]; then
 	./bootstrap
 	if [ $? -gt 0 ]; then
@@ -115,32 +130,35 @@ fi
 #
 # Setup the configuration so that the system search path looks in lib and include ahead of the standard C libraries
 #
-#./configure CC=c99 CFLAGS="-qlanglvl=extc1x -qascii -D_OPEN_THREADS=3 -D_UNIX03_SOURCE=1 -DNSIG=39 -D_AE_BIMODAL=1 -D_ALL_SOURCE -D_ENHANCED_ASCII_EXT=0xFFFFFFFF -D_OPEN_SYS_FILE_EXT=1 -D_OPEN_SYS_SOCK_IPV6 -D_UNIX03_THREADS -D_UNIX03_WITHDRAWN -D_XOPEN_SOURCE=600 -D_XOPEN_SOURCE_EXTENDED -qfloat=ieee -qnose -I${M4_ROOT}/${M4_VRM}/lib,${DELTA_ROOT}/include,/usr/include"
-./configure CC=c99 CFLAGS="-qlanglvl=extc1x -qascii -D_OPEN_THREADS=3 -D_UNIX03_SOURCE=1 -DNSIG=39 -D_AE_BIMODAL=1 -D_XOPEN_SOURCE_EXTENDED -D_ALL_SOURCE -D_ENHANCED_ASCII_EXT=0xFFFFFFFF -D_OPEN_SYS_FILE_EXT=1 -D_OPEN_SYS_SOCK_IPV6 -D_XOPEN_SOURCE=600 -D_XOPEN_SOURCE_EXTENDED  -qnose -qfloat=ieee -I${M4_ROOT}/${M4_VRM}/lib,${DELTA_ROOT}/include,/usr/include"
+./configure CC=c99 CFLAGS="-qlanglvl=extc1x -qascii -D_OPEN_THREADS=3 -D_UNIX03_SOURCE=1 -DNSIG=39 -D_AE_BIMODAL=1 -D_XOPEN_SOURCE_EXTENDED -D_ALL_SOURCE -D_ENHANCED_ASCII_EXT=0xFFFFFFFF -D_OPEN_SYS_FILE_EXT=1 -D_OPEN_SYS_SOCK_IPV6 -D_XOPEN_SOURCE=600 -D_XOPEN_SOURCE_EXTENDED  -qnose -qfloat=ieee -I${M4_ROOT}/${M4_VRM}/lib,${DELTA_ROOT}/include,/usr/include" --prefix="${M4_INSTALL_PREFIX}" --disable-dependency-tracking
 if [ $? -gt 0 ]; then
 	echo "Configure of M4 tree failed." >&2
 	exit 16
 fi
 
-cd "${M4_ROOT}/${M4_VRM}"
-make
-if [ $? -gt 0 ]; then
+cd "${M4_ROOT}/${M4_VRM}" || exit 99
+if ! make ; then
 	echo "MAKE of M4 tree failed." >&2
 	exit 16
 fi
 
-
 cd "${DELTA_ROOT}/tests"
 export PATH="${M4_ROOT}/${M4_VRM}/src:${PATH}"
 
-./runbasic.sh
-if [ $? -gt 0 ]; then
+if ! ./runbasic.sh ; then
 	echo "Basic test of M4 failed." >&2
 	exit 16
 fi
-./runexamples.sh
-if [ $? -gt 0 ]; then
+if ! ./runexamples.sh ; then
 	echo "Example tests of M4 failed." >&2
 	exit 16
 fi
+
+if ! make install ; then
+	echo "Make install of M4 failed." >&2
+	exit 16
+fi
+
+echo "M4 installed into ${M4_INSTALL_PREFIX}"
+
 exit 0
--- setenv.sh
+++ setenv.sh
@@ -10,6 +10,27 @@ else
 	export _TAG_REDIR_IN="txt"
 	export _TAG_REDIR_OUT="txt"
 
+# Note to build m4 you need to either use a tarball that is pre-configured
+# or clone the code from git.
+# 
+# If you use the pre-configured m4 source tarball, you need a 'bootstrap' m4
+# and you need curl to pull down the tarball
+#
+# If you clone the code from git, you need to already have the Autotools installed
+# on your system
+#
+# Specifying M4_VRM of m4-1.4.19 will take the 'tarball' path
+# Specifying M4_VRM of m4 will take the 'git' path
+#
+#export M4_VRM="m4"
+#export AUTOTOOLS_MIRROR="https://github.com/autotools-mirror"
+#export M4_BRANCH="branch-1.4"
+
+	export M4_URL="http://ftp.gnu.org/gnu/m4/"
 	export M4_VRM="m4-1.4.19"
 	export M4_ROOT="${PWD}"
+	export fsroot=$( basename $HOME )
+	export M4_BOOTSTRAP_ROOT="/${fsroot}/m4boot"
+	export M4_INSTALL_PREFIX="/${fsroot}/m4prod"   
+
 fi
