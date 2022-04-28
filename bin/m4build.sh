887ec241567812d3efa58c7e412032162faf78a0
Mike Fulton <mikefultonpersonal@gmail.com>
2022-04-28 13:06:31 -0400

    clean up options

--- bin/m4build.sh
+++ bin/m4build.sh
@@ -138,7 +138,7 @@ fi
 #
 # Setup the configuration so that the system search path looks in lib and include ahead of the standard C libraries
 #
-./configure CC=c99 CFLAGS="-qlanglvl=extc1x -qascii -D_OPEN_THREADS=3 -D_UNIX03_SOURCE=1 -DNSIG=39 -D_AE_BIMODAL=1 -D_XOPEN_SOURCE_EXTENDED -D_ALL_SOURCE -D_ENHANCED_ASCII_EXT=0xFFFFFFFF -D_OPEN_SYS_FILE_EXT=1 -D_OPEN_SYS_SOCK_IPV6 -D_XOPEN_SOURCE=600 -D_XOPEN_SOURCE_EXTENDED  -qnose -qfloat=ieee -I${M4_ROOT}/${M4_VRM}/lib,${DELTA_ROOT}/include,/usr/include" --prefix="${M4_INSTALL_PREFIX}" --disable-dependency-tracking
+./configure --prefix="${M4_INSTALL_PREFIX}"
 if [ $? -gt 0 ]; then
 	echo "Configure of M4 tree failed." >&2
 	exit 16
@@ -146,6 +146,11 @@ fi
 
 cd "${M4_ROOT}/${M4_VRM}" || exit 99
 if ! make ; then
+	echo "MAKE of M4 tree failed." >&2
+	exit 16
+fi
+
+if ! make check ; then
 	echo "MAKE of M4 tree failed." >&2
 	exit 16
 fi
