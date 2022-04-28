4c57f1053367a448f424114bf666c829b5343000
Mike Fulton <mikefultonpersonal@gmail.com>
2022-04-28 14:37:39 -0400

    move to common patching

--- bin/m4build.sh
+++ bin/m4build.sh
@@ -112,24 +112,9 @@ cd "${M4_ROOT}/${M4_VRM}" || exit 99
 #
 # Apply patches
 #
-if true ; then
-	if ! manage_patches.sh ; then
-		echo "Unable to patch M4 tree" >&2
-		exit 16
-	fi
-else
-	if [ "${M4_VRM}" = "m4-1.4.19" ]; then
-		patch -c lib/canonicalize-lgpl.c <${M4_ROOT}/patches/canonicalize-lgpl.patch
-		if [ $? -gt 0 ]; then
-			echo "Patch of M4 tree failed (canonicalize-lgpl)." >&2
-			exit 16
-		fi
-		patch -c src/builtin.c <${M4_ROOT}/patches/builtin.patch
-		if [ $? -gt 0 ]; then
-			echo "Patch of M4 tree failed (builtin)." >&2
-			exit 16
-		fi
-	fi
+if ! managepatches.sh ; then
+	echo "Unable to patch M4 tree" >&2
+	exit 16
 fi
 
 export PATH="${M4_BOOTSTRAP_ROOT}/bin:${PATH}"
