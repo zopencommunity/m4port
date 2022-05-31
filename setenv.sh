d68e525ab6118383fe223a2dc0d6430185a3a40d
Mike Fulton <mikefultonpersonal@gmail.com>
2022-05-31 19:13:22 -0400

    fix system header location for xlclang and add more dependencies for git build

--- setenv.sh
+++ setenv.sh
@@ -16,7 +16,7 @@ export PORT_TARBALL_DEPS="curl gzip make m4"
 
 PORT_GIT_DIR=m4
 export PORT_GIT_URL="https://github.com/autotools-mirror/${PORT_GIT_DIR}.git"
-export PORT_GIT_DEPS="git make m4 help2man perl makeinfo xz"
+export PORT_GIT_DEPS="git make m4 help2man perl makeinfo xz autoconf automake"
 export PORT_GIT_BRANCH="branch-1.6"
 
 export PORT_EXTRA_LDFLAGS=""
@@ -24,9 +24,9 @@ export PORT_EXTRA_LDFLAGS=""
 rm patches
 if [ "${PORT_TYPE}x" = "TARBALLx" ]; then
 	export PORT_BOOTSTRAP=skip
-	export PORT_EXTRA_CFLAGS="-qnose -I${PORT_ROOT}/${PORT_TARBALL_DIR}/lib,${PORT_ROOT}/patches/PR1/include,/usr/include"
+	export PORT_EXTRA_CFLAGS="-qnose -I${PORT_ROOT}/${PORT_TARBALL_DIR}/lib,${PORT_ROOT}/patches/PR1/include,/usr/include/le"
 	ln -s tarball-patches patches
 else
-	export PORT_EXTRA_CFLAGS="-qnose -I${PORT_ROOT}/${PORT_GIT_DIR}/lib,${PORT_ROOT}/patches/PR1/include,/usr/include"
+	export PORT_EXTRA_CFLAGS="-qnose -fgnu89-inline -I${PORT_ROOT}/${PORT_GIT_DIR}/lib,${PORT_ROOT}/patches/PR1/include,/usr/include/le"
 	ln -s git-patches patches
 fi
