ebae7457a8d8df6dad74313d13309401d43bd60f
Mike Fulton <mikefultonpersonal@gmail.com>
2022-06-03 15:24:39 -0400

    move to proper git clone location

--- setenv.sh
+++ setenv.sh
@@ -15,9 +15,9 @@ export PORT_TARBALL_URL="https://ftp.gnu.org/gnu/m4/${PORT_TARBALL_DIR}.tar.gz"
 export PORT_TARBALL_DEPS="curl gzip make m4"
 
 PORT_GIT_DIR=m4
-export PORT_GIT_URL="https://github.com/autotools-mirror/${PORT_GIT_DIR}.git"
-export PORT_GIT_DEPS="git make m4 help2man perl makeinfo xz autoconf automake"
-export PORT_GIT_BRANCH="branch-1.6"
+export PORT_GIT_URL="git://git.savannah.gnu.org/${PORT_GIT_DIR}.git"
+export PORT_GIT_DEPS="git make m4 help2man perl makeinfo xz autoconf automake gettext"
+export PORT_GIT_BRANCH="branch-1.4"
 
 export PORT_EXTRA_LDFLAGS=""
 
