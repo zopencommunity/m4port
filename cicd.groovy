3b9de6e350d3334031fa276083fa94ceac402cc3
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 08:46:59 -0400

    Add description to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux') 
 {
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'm4port'), string(name: 'DESCRIPTION', 'm4port' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'm4port'), string(name: 'DESCRIPTION', 'M4 is an implementation of the traditional Unix macro processor.' )]
         }
 }
