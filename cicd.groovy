13d8cf5ead29aa25f6b815df599ad674eb41ed17
Igor Todorovski <itodorov@ca.ibm.com>
2022-09-19 11:41:04 -0400

    Enable testing for m4

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,8 @@ node('linux')
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/m4port.git'), string(name: 'PORT_DESCRIPTION', value: 'M4 is an implementation of the traditional Unix macro processor.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/m4port.git'), 
+		string(name: 'PORT_DESCRIPTION', value: 'M4 is an implementation of the traditional Unix macro processor.' ),
+		booleanParam(name: 'RUN_TESTS', value: true) ]
         }
 }
