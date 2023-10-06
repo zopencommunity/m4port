e90fe997a64906f9d3c987287819329bde799e15
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2023-10-06 15:32:02 -0400

    Update cicd-dev.groovy

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -4,7 +4,7 @@ node('linux')
   stage ('Poll') {
                // Poll from upstream:
                checkout([
-                       $class: 'GitSCM',: 'GitSCM',
+                       $class: 'GitSCM',
                        branches: [[name: '*/branch-1.4']],
                        doGenerateSubmoduleConfigurations: false,
                        extensions: [],
